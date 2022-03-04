3f3de48629837feb0bc839e0a20b2d7dbe959cf8
DanielIppo <91868349+DanielIppo@users.noreply.github.com>
2022-03-04 11:28:58 +0100

    Update main.cpp

--- C++/Aereoporto/main.cpp
+++ C++/Aereoporto/main.cpp
@@ -13,7 +13,6 @@ le informazioni di un determinato volo dato in input;
 codice volo, Città, orario, voli dove lo stato è B;
 Visualizzare le informazioni dei voli in partenza per una città data in input
 Dato in input (?) l'orario attuale/ acquisito l'orario di sistema visualizzare i voli il cui orario programmato è superato e non è ancora atterrato/partito e modicarlo in D.
-
 */
 
 #ifdef _WIN32
@@ -34,70 +33,110 @@ string ColoreDefault = "\x1b[0m";
 string Bold = "\x1b[1m";
 string AttributiStringOff = "\x1b[0m";
 
-void Clear(){
-    #if _WIN32
+void Clear() {
+#if _WIN32
     system("cls");
-    #else
+#else
     system("clear");
-    #endif
+#endif
 }
 
 
-string codice[5] = {"AZ_297856","FR_654355","U2_998612","LH_897132","AF_683546"};
-string compagniaAerea[5] = {"Alitalia","Ryanair","Easyjet","Lufthansa","Air France"};
-char partenzaArrivo[5] = {'P','A','P','P','A'};
-int ora [5] = {12, 15, 22, 10, 16};
-int minuti [5] = {30, 20, 00, 50, 55};
+string codice[5] = { "AZ_297856","FR_654355","U2_998612","LH_897132","AF_683546" };
+string compagniaAerea[5] = { "Alitalia","Ryanair","Easyjet","Lufthansa","Air France" };
+char partenzaArrivo[5] = { 'P','A','P','P','A' };
+int ora[5] = { 12, 15, 22, 10, 16 };
+int minuti[5] = { 30, 20, 00, 50, 55 };
+int indice;
+
+time_t theTime = time(NULL);
+struct tm* aTime = localtime(&theTime);
+int ore = (aTime->tm_hour) + 1;
+int minuti = aTime->tm_min;
 
 
-//time_t theTime = time(NULL);
+class Volo {
+public:
 
-
-
-class Volo{
-    public:
-    
     string scelta;
-    
-    void home(){
-        cout <<Rosso<<"####################################"<<endl;
-        cout <<"########## Agenzia Viaggi ##########"<<endl;
-        cout <<"####################################"<<ColoreDefault<<endl<<endl;
-        
-        cout << Bold <<"Che cosa vorresti fare?\n"<<Blue <<"1)Verificare lo stato di un volo.\n2)Vedere se si è in tempo.\n\n"<< ColoreDefault <<AttributiStringOff;
+
+    void home() {
+        cout << Rosso << "####################################" << endl;
+        cout << "########## Agenzia Viaggi ##########" << endl;
+        cout << "####################################" << ColoreDefault << endl << endl;
+
+        cout << Bold << "Che cosa vorresti fare?\n" << Blue << "1)Verificare lo stato di un volo.\n2)Vedere se è già partito.\n\n" << ColoreDefault << AttributiStringOff;
         cin >> scelta;
-        if(scelta == "1" || scelta == "uno" || scelta == "Uno"){
+        if (scelta == "1" || scelta == "uno" || scelta == "Uno") {
             infoVoli();
         }
-        else if(scelta == "2" || scelta == "due" || scelta == "Due"){
-            //tempoAlVolo
+        else if (scelta == "2" || scelta == "due" || scelta == "Due") {
+            tempoAlVolo();
         }
     }
-    
-    void infoVoli(){
+
+    void tempoAlVolo() {
         Clear();
-        cout <<Rosso<<"####################################"<<endl;
-        cout <<"########## Agenzia Viaggi ##########"<<endl;
-        cout <<"####################################"<<ColoreDefault<<endl<<endl;
-        cout <<Bold<<"Inserisci il tuo codice volo: "<<AttributiStringOff;
+        cout << Rosso << "####################################" << endl;
+        cout << "########## Agenzia Viaggi ##########" << endl;
+        cout << "####################################" << ColoreDefault << endl << endl;
+        cout << Bold << "Inserisci il tuo codice volo: " << AttributiStringOff;
         cin >> scelta;
+
+        verificaCodice(false);
+        //verificaOrario();
+
+    }
+
+    void infoVoli() {
+        Clear();
+        cout << Rosso << "####################################" << endl;
+        cout << "########## Agenzia Viaggi ##########" << endl;
+        cout << "####################################" << ColoreDefault << endl << endl;
+        cout << Bold << "Inserisci il tuo codice volo: " << AttributiStringOff;
+        cin >> scelta;
+
+        verificaCodice(false);
+
+        cout << "Codice volo: " << Magenta << codice[indice] << ColoreDefault << "\nCompagniaAerea: " << Verde << compagniaAerea[indice] << ColoreDefault << "\nStato: " << Blue << partenzaArrivo[indice] << ColoreDefault << "\nOrario: " << Giallo << ora[indice] << ":" << minuti[indice] << ColoreDefault << endl;
+        Sleep(5000);
+        Clear();
+        home();
+    }
+
+
+    bool verificaOrario(bool flag = false) {
+
         
-        for(int i = 0; i<5;i++){
-            if(scelta == codice[i]){
-                cout << "Codice volo: "<<Magenta<<codice[i]<<ColoreDefault<<"\nCompagniaAerea: "<<Verde<<compagniaAerea[i]<<ColoreDefault<<"\nStato: "<<Blue<<partenzaArrivo[i]<<ColoreDefault<<"\nOrario: "<<Giallo<<ora[i]<<":"<<minuti[i]<<ColoreDefault<<endl;
-                sleep(5);
-                Clear();
-                home();
-            } else {
-                
+
+
+    }
+
+
+    bool verificaCodice(bool flag = false) {
+
+        for (int i = 0; i < 5; i++) {
+            if (scelta == codice[i]) {
+                indice = i;
+                flag = true;
+                return true;
             }
         }
+        if (flag == false) {
+            cout << endl << "Hai sbagliato a digitare, riprova...";
+            Sleep(4000);
+            Clear();
+            home();
+            return false();
+        }
     }
+
+
 };
 
 
 
-int main(){
-Volo volo;
+int main() {
+    Volo volo;
     volo.home();
 }
